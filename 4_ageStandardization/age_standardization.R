4ca171a60293fa9185c58a7db99bb780e6dab0a0
Danielle Newby <danielle.newby@ndorms.ox.ac.uk>
2024-03-08 09:35:56 +0000

    update

--- 4_ageStandardization/age_standardization.R
+++ 4_ageStandardization/age_standardization.R
@@ -654,7 +654,57 @@ dev.off()
 }
 
 
+# for all head and nceck cancers combined
 
+agestandardizedinc_final_han <- agestandardizedinc_final %>%
+  filter(Sex != "Both" ,
+         Cancer == "Head & Neck" |
+           Cancer == "Hypopharynx" |
+           Cancer == "Larynx" |
+           Cancer == "Nasal Cavity & Sinus" |
+           Cancer == "Nasopharynx" |
+           Cancer == "Oral Cavity" |
+           Cancer == "Oropharynx" |
+           Cancer == "Salivary Gland" |
+           Cancer == "Tongue"  )
+
+
+incidenceFigureData5 <- agestandardizedinc_final_han %>%         
+  mutate(database_name = "CPRD GOLD") %>% 
+  ggplot(aes(x = Subgroup,
+             y = `Std Rate (per 1e+05)`,
+             group = Sex )) +
+  geom_line(color = "black", size = 0.25) +
+  scale_colour_manual(values = c("#ED0000FF", "#00468BFF", "#0099B4FF", "#42B540FF", "#925E9FFF", "#FDAF91FF", "#AD002AFF", "grey")) + #blue, #red, #lightblue, #green, purple, peach, dark read, gry
+  scale_fill_manual(values = c("#ED0000FF", "#00468BFF", "#0099B4FF", "#42B540FF", "#925E9FFF", "#FDAF91FF", "#AD002AFF", "grey")) +
+  geom_ribbon(aes(ymin = `95% LCL (Std)`, 
+                  ymax = `95% UCL (Std)`, 
+                  fill = Sex), alpha = .15, color = NA, show.legend = FALSE) +
+  geom_point(aes(shape = Sex, fill = Sex),size = 2) +
+  scale_shape_manual(values = c(24,21)) +
+  theme(axis.text.x = element_text(angle = 45, hjust=1),
+        panel.border = element_rect(color = "black", fill = NA, size = 0.6), 
+        strip.background = element_rect(color = "black", size = 0.6) ,
+        panel.background = element_blank() ,
+        axis.line = element_line(colour = "black", size = 0.6) ,
+        #panel.spacing.x = unit(0.1,"line"),
+        panel.grid.major = element_line(color = "grey", size = 0.2, linetype = "dashed")) +
+  geom_vline(xintercept = as.numeric(as.Date("2004-04-01")), linetype="dotted", colour = "#ED0000FF", size = 0.8) +
+  labs(x = "Calendar year",
+       y = "Age Standardized Incidence rate per 100000 person-years",
+       col = "Sex",
+       shape = "Sex",
+       fill = "Sex") +
+  scale_x_date(labels = date_format("%Y"), breaks = date_breaks("2 years"),
+               expand = c(0.06,1)) +
+  facet_wrap(~ Cancer, scales = "free_y", ncol = 3)
+
+
+plotname <- paste0("/FIGUREX_", names(table(incidence_estimates4$outcome_cohort_name)[i]),"_IRsSEX_ageadjusted.tiff")
+tiff(paste0(datapath , plotname),
+     width = 6, height = 5 , units = "in", res = 1200)
+print(incidenceFigureData5, newpage = FALSE)
+dev.off()
 
 
 ################################## Prevalence #######

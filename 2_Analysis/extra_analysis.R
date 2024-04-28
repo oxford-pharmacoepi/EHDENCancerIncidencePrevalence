f0d5257bf3faae88418c2f95a242b8d7b7de82ad
daniellenewby <danielleanne.newby@gmail.com>
2024-04-28 22:50:59 +0100

    update

--- 2_Analysis/extra_analysis.R
+++ 2_Analysis/extra_analysis.R
@@ -14,19 +14,22 @@ lung_cancer_prevalent_cohorts <- CDMConnector::readCohortSet(here::here(
   "extra_analysis"
   ))
 
+#tablenames
+lc_outcome_table_name <-paste0(outcome_table_stem,"_olc") # for incidence
+lc_prevalent_table_name <-paste0(outcome_table_stem,"_plc") # for prevalence
 
 #instantiate the lung cancer cohorts
 # incidence
 cdm <- CDMConnector::generateCohortSet(cdm, 
-                                       cohortSet = outcome_cohorts,
-                                       name = outcome_table_name,
+                                       cohortSet = lung_cancer_outcome_cohorts,
+                                       name = lc_outcome_table_name,
                                        overwrite = TRUE
 )
 
 # prevalence
 cdm <- CDMConnector::generateCohortSet(cdm = cdm, 
-                                       cohortSet = prevalent_cohorts,
-                                       name = prevalent_table_name,
+                                       cohortSet = lung_cancer_prevalent_cohorts,
+                                       name = lc_prevalent_table_name,
                                        overwrite = TRUE
                                        
 )

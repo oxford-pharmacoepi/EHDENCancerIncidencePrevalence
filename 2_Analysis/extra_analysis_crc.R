32d8c816da4f176e638855f4d4bd5a9fab4a6177
Danielle Newby <danielle.newby@ndorms.ox.ac.uk>
2025-01-07 10:10:58 +0000

    adding in extra analysis

--- 2_Analysis/extra_analysis_crc.R
+++ 2_Analysis/extra_analysis_crc.R
@@ -7,13 +7,145 @@
 crc_cancer_outcome_cohorts <- CDMConnector::readCohortSet(here::here(
   "1_InstantiateCohorts",
   "OutcomeCohorts",
-  "crc_extra_analysis"
+  "extra_analysis_crc"
 ))
 
+#tablenames
+crc_outcome_table_name <-paste0(outcome_table_stem,"_ocrc") # for incidence
+
+
+#instantiate the crc cancer cohorts
+# incidence
+cdm <- CDMConnector::generateCohortSet(cdm, 
+                                       cohortSet = crc_cancer_outcome_cohorts,
+                                       name = crc_outcome_table_name,
+                                       overwrite = TRUE
+)
 
 
 
 
 
 
-# 2 # analysis with smaller age groups
+# 2 # analysis with smaller age groups (add into denominator)
+
+# take denominator (should already be run)
+cdm$denominator <- generateDenominatorCohortSet(
+  cdm = cdm,
+  startDate = as.Date(studyStartDate),
+  endDate = as.Date(studyEndDate),
+  ageGroup =list(
+    c(18, 150),
+    c(18, 29),
+    c(30, 39),
+    c(40, 49),
+    c(50, 59),
+    c(60, 69),
+    c(70, 79),
+    c(80, 89),
+    c(90, 150),
+    
+    c(18, 24),
+    c(25, 29),
+    c(30, 34),
+    c(35, 39),
+    c(40, 44),
+    c(45, 49),
+    c(50, 54),
+    c(55, 59),
+    c(60, 64),
+    c(65, 69),
+    c(70, 74),
+    c(75, 79),
+    c(80, 84),
+    c(85, 89)
+    
+    
+    
+  ),
+  sex = c("Male", "Female", "Both"),
+  daysPriorHistory = 365,
+  verbose = TRUE
+)
+
+
+# run incidence
+
+inc <- estimateIncidence(
+  cdm = cdm,
+  denominatorTable = "denominator",
+  outcomeTable = crc_outcome_table_name,
+  denominatorCohortId = NULL,
+  outcomeCohortId = crc_cancer_outcome_cohorts$cohort_definition_id,
+  outcomeCohortName = crc_cancer_outcome_cohorts$cohort_name,
+  interval = "years", 
+  outcomeWashout = NULL,
+  repeatedEvents = FALSE,
+  completeDatabaseIntervals = TRUE,
+  minCellCount = 5,
+  returnParticipants = FALSE,
+  verbose = TRUE
+)
+
+
+
+# Get the results ----------------
+
+
+study_results <- gatherIncidencePrevalenceResults(cdm =cdm, 
+                                                  resultList=list(inc),
+                                                  databaseName = db.name)
+
+
+
+# Export the results -----
+
+
+exportIncidencePrevalenceResults(result=study_results,
+                                 zipName= paste0(db.name, "IPResults_extra_crc"),
+                                 outputFolder=here::here("Results", db.name))
+
+
+# age standardization
+
+# for age standardization we need results without obscuring therefore if
+# age standardization required it will run this code and save the results
+
+agestandardization == TRUE
+
+if (agestandardization == TRUE) {
+  
+  inc <- estimateIncidence(
+    cdm = cdm,
+    denominatorTable = "denominator",
+    outcomeTable = crc_outcome_table_name,
+    denominatorCohortId = NULL,
+    outcomeCohortId = crc_cancer_outcome_cohorts$cohort_definition_id,
+    outcomeCohortName = crc_cancer_outcome_cohorts$cohort_name,
+    interval = "years", 
+    outcomeWashout = NULL,
+    repeatedEvents = FALSE,
+    completeDatabaseIntervals = TRUE,
+    minCellCount = 0,
+    returnParticipants = FALSE,
+    verbose = TRUE
+
+  )
+  
+  
+  # Get the results ----------------
+  study_results1 <- gatherIncidencePrevalenceResults(cdm =cdm, 
+                                                     resultList=list(inc),
+                                                     databaseName = db.name)
+  
+  exportIncidencePrevalenceResults(result=study_results1,
+                                   zipName= paste0(db.name, "IPResultsAgeStandardization_extra_crc"),
+                                   outputFolder=here::here("Results", db.name))
+  
+
+}
+
+
+
+
+

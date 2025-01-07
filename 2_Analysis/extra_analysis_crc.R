e3a45896b47dd827333900589e25ad5ae1e82a18
Danielle Newby <danielle.newby@ndorms.ox.ac.uk>
2025-01-07 09:51:03 +0000

    adding in crc additional analysis

---
+++ 2_Analysis/extra_analysis_crc.R
@@ -0,0 +1,19 @@
+# reviewer revisions
+
+
+# 1 # analysis by different regions on crc
+
+# read in crc cancer cohort colon and rectal
+crc_cancer_outcome_cohorts <- CDMConnector::readCohortSet(here::here(
+  "1_InstantiateCohorts",
+  "OutcomeCohorts",
+  "crc_extra_analysis"
+))
+
+
+
+
+
+
+
+# 2 # analysis with smaller age groups
